8a66377619b2c8ad1378466f98819082de4744f6
erhansakarya <erhansakaryaa@gmail.com>
2018-09-28 13:27:34 +0300

    repository was created.

---
+++ cc3200_c_sdk/PRCM/1-lowLayer/LL_prcm.h
@@ -0,0 +1,23 @@
+/** @file rcc.h
+*
+* @brief
+* This module provides reset and clock management
+*
+* @par
+*
+*/
+
+#ifndef _RCC_H
+#define _RCC_H
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+  
+extern void initRcc(void);
+    
+#ifdef __cplusplus
+  }
+#endif
+
+#endif /* _RCC_H */
---
+++ cc3200_c_sdk/PRCM/main.c
@@ -0,0 +1,51 @@
+/** @file main.c
+*
+* @brief A description of the module's purpose
+*  
+* @par
+*
+*/
+
+// Standard includes
+#include <stdio.h>
+#include <stdlib.h>
+
+// driverlib includes
+#include "hw_types.h"                   // Includes data types and macros for hardware access, both direct and via the bit-band region.
+#include "hw_ints.h"                    // Includes defines for the interrupt assignments.
+#include "hw_memmap.h"                  // Includes the base address of the memories and peripherals.
+#include "hw_common_reg.h"              // Includes register offsets.
+#include "interrupt.h"                  // Interrupt driver.
+#include "hw_apps_rcm.h"                // ResetClockManager driver.
+#include "prcm.h"                       // Peripheral Clock and Reset Control Driver.
+#include "rom.h"                        // ROM Tables and APIs
+#include "rom_map.h"                    // ROM Macros for APIs
+#include "utils.h"                      // Utils Delay Driver.
+
+// low layer includes
+
+// mid layer includes
+
+
+//*****************************************************************************
+//                      LOCAL FUNCTION PROTOTYPES                           
+//*****************************************************************************
+
+//*****************************************************************************
+//                      LOCAL FUNCTION DEFINITIONS                         
+//*****************************************************************************
+
+int
+main()
+{
+    //handleAcController();
+    
+    return 0;
+}
+
+//*****************************************************************************
+//
+// Close the Doxygen group.
+//! @}
+//
+//*****************************************************************************
