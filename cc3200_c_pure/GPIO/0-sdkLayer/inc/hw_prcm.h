72e061e7888dd62ee1a964a61994f68ce6878a99
erhansakarya <erhansakaryaa@gmail.com>
2018-10-19 09:05:09 +0300

    GPIO project was initialized.

---
+++ cc3200_c_pure/GPIO/0-sdkLayer/inc/hw_prcm.h
@@ -0,0 +1,102 @@
+#ifndef __HW_PRCM_H__
+#define __HW_PRCM_H__
+
+//*****************************************************************************
+//
+// defines for the PRCM (Power Clock Reset Manager) register offsets.
+//
+//*****************************************************************************
+typedef struct {                // offsets
+  uint32_t CAMCLKCFG;           // 0x00
+  uint32_t CAMCLKEN;            // 0x04
+  uint32_t CAMSWRST;            // 0x08
+  uint16_t RESERVED;            // 0x12
+  uint32_t MCASPCLKEN;          // 0x14
+  uint32_t MCASPSWRST;          // 0x18
+  uint16_t SDIOMCLKCFG;         // 0x20
+  uint32_t SDIOMCLKEN;          // 0x24
+  uint32_t SDIOMSWRST;          // 0x28
+  uint32_t APSPICLKCFG;         // 0x2C
+  uint32_t APSPICLKEN;          // 0x30
+  uint32_t APSPISWRST;          // 0x34
+  uint32_t RESERVED_0;          // 0x38
+  uint32_t RESERVED_1;          // 0x3C
+  uint32_t RESERVED_2;          // 0x40
+  uint32_t RESERVED_3;          // 0x44
+  uint32_t DMACLKEN;            // 0x48
+  uint32_t DMASWRST;            // 0x4C
+  uint32_t GPIO0CLKEN;          // 0x50
+  uint32_t GPIO0SWRST;          // 0x54
+  uint32_t GPIO1CLKEN;          // 0x58
+  uint32_t GPIO1SWRST;          // 0x5C
+  uint32_t GPIO2CLKEN;          // 0x60
+  uint32_t GPIO2SWRST;          // 0x64
+  uint32_t GPIO3CLKEN;          // 0x68
+  uint32_t GPIO3SWRST;          // 0x6C
+  uint32_t GPIO4CLKEN;          // 0x70
+  uint32_t GPIO4SWRST;          // 0x74
+  uint32_t WDTCLKEN;            // 0x78
+  uint32_t WDTSWRST;            // 0x7C
+  uint32_t UART0CLKEN;          // 0x80
+  uint32_t UART0SWRST;          // 0x84
+  uint32_t UART1CLKEN;          // 0x88
+  uint32_t UART1SWRST;          // 0x8C
+  uint32_t GPT0CLKCFG;          // 0x90
+  uint32_t GPT0SWRST;           // 0x94
+  uint32_t GPT1CLKEN;           // 0x98
+  uint32_t GPT1SWRST;           // 0x9C
+  uint32_t GPT2CLKEN;           // 0xA0
+  uint32_t GPT2SWRST;           // 0xA4
+  uint32_t GPT3CLKEN;           // 0xA8
+  uint32_t GPT3SWRST;           // 0xAC
+  uint32_t MCASPCLKCFG0;        // 0xB0
+  uint32_t MCASPCLKCFG1;        // 0xB4
+  uint32_t RESERVED_4;          // 0xB8
+  uint32_t RESERVED_5;          // 0xBC
+  uint32_t RESERVED_6;          // 0xC0
+  uint32_t RESERVED_7;          // 0xC4
+  uint32_t RESERVED_8;          // 0xC8
+  uint32_t RESERVED_9;          // 0xCC
+  uint32_t RESERVED_10;         // 0xD0
+  uint32_t RESERVED_11;         // 0xD4
+  uint32_t I2CLCKEN;            // 0xD8
+  uint32_t I2CSWRST;            // 0xDC
+  uint32_t RESERVED_12;         // 0xE0
+  uint32_t LPDSREQ;             // 0xE4
+  uint32_t RESERVED_13;         // 0xE8
+  uint32_t TURBOREQ;            // 0xEC
+  uint32_t RESERVED_14;         // 0xF0
+  uint32_t RESERVED_15;         // 0xF4
+  uint32_t RESERVED_16;         // 0xF8
+  uint32_t RESERVED_17;         // 0xFC
+  uint32_t RESERVED_18;         // 0x100
+  uint32_t RESERVED_19;         // 0x104
+  uint32_t DSLPWAKECFG;         // 0x108
+  uint32_t DSLPTIMRCFG;         // 0x10C
+  uint32_t SLPWAKEEN;           // 0x110
+  uint32_t SLPTMRCFG;           // 0x114
+  uint32_t WAKENWP;             // 0x118
+  uint32_t RESERVED_20;         // 0x11C
+  uint32_t RCM_IS;              // 0x120
+  uint32_t RCM_IEN;             // 0x124
+  
+}PRCM_t;
+
+// PRCM Register Structure
+#define PRCM  (PRCM_t volatile *GPRCM_BASE)
+
+// enum
+typedef enum {
+  ENABLE,
+  DISABLE,
+  RUN_MODE              = (uint32_t)0x01,
+  SLEEP_MODE            = (uint32_t)0x100,
+  DEEPSLEEP_MODE        = (uint32_t)0x10000,
+
+} PERIPH_EN_DIS_t;
+
+// functions
+extern int8_t PRCM_GetPeriphClkState(uint32_t periph, uint32_t mode);
+extern int8_t PRCM_EnDisPeriphClkState(uint32_t periph, const uint8_t enableDisable, uint32_t mode);
+
+#endif // __HW_PRCM_H__
---
+++ cc3200_c_pure/GPIO/main.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// includes for hardware
+#include "hw_types.h"
+#include "hw_memmap.h"
+#include "hw_prcm.h"
+#include "hw_gpio.h"
+
+void boardInit(void)
+{
+   // set port A1.1 as output (RED LED GPIO_09)
+   HWREG(GPIOA1_BASE + GPIO_O_GPIO_DIR)|= 0x00000002 ;
+   // switch off the pin
+   HWREG(GPIOA1_BASE + GPIO_O_GPIO_DATA + (0x00000002<<2)) = 0x00000000;
+   // set port A1.2 as output (YELLOW LED GPIO_10)
+   HWREG(GPIOA1_BASE + GPIO_O_GPIO_DIR)|= 0x00000004 ;
+   // switch off the pin
+   HWREG(GPIOA1_BASE + GPIO_O_GPIO_DATA + (0x00000004<<2)) = 0x00000000;
+   // set port A1.3 as output (GREEN LED GPIO_11)
+   HWREG(GPIOA1_BASE + GPIO_O_GPIO_DIR)|= 0x00000008 ;
+   // switch off the pin
+   HWREG(GPIOA1_BASE + GPIO_O_GPIO_DATA + (0x00000008<<2)) = 0x00000000;
+}
+
+void delayByCount(float tellerwaarde)
+{
+    int teller = 0;
+    while (teller < tellerwaarde)
+    {
+        teller++;
+    }
+}
+
+/*
+ * main.c
+ */
+void main(void)
+{
+    boardInit();
+    while(1)
+    {
+        //Blink RED
+        delayByCount(200000);
+        HWREG(GPIOA1_BASE + GPIO_O_GPIO_DATA + (0x00000002<<2)) = 0X00000002;
+        delayByCount(200000);
+        HWREG(GPIOA1_BASE + GPIO_O_GPIO_DATA + (0x00000002<<2)) = 0X00000000;
+        //BLINK YELLOW
+        delayByCount(200000);
+        HWREG(GPIOA1_BASE + GPIO_O_GPIO_DATA + (0x00000004<<2)) = 0X00000004;
+        delayByCount(200000);
+        HWREG(GPIOA1_BASE + GPIO_O_GPIO_DATA + (0x00000004<<2)) = 0X00000000;
+        //Blink GREEN
+        delayByCount(200000);
+        HWREG(GPIOA1_BASE + GPIO_O_GPIO_DATA + (0x00000008<<2)) = 0X00000008;
+        delayByCount(200000);
+        HWREG(GPIOA1_BASE + GPIO_O_GPIO_DATA + (0x00000008<<2)) = 0X00000000;
+   }
+}
