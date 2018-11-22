bcc1aaad4661ea6b048c764eea3b10e4d87f62ff
erhansakarya <erhansakaryaa@gmail.com>
2018-11-22 20:09:20 +0300

    button support was added.

--- stm32f1_keil_mdk/GPIO/main.c
+++ stm32f1_keil_mdk/GPIO/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 
 #include "Board_LED.h"
+#include "Board_Buttons.h"
 
 void delay(void){
 	uint32_t i = 0;
@@ -9,13 +10,20 @@ void delay(void){
 }
 
 int main(void){
+	uint8_t state = 0;
+	
 	LED_Initialize();
+	Buttons_Initialize();
 	
 	while(1){
-		LED_On(0);
-		delay();
-		LED_Off(0);
-		delay();
+		if(Buttons_GetState()){
+			state = !state;
+			(state) ? LED_On(0): LED_Off(0);
+			//delay();
+		}
+		
+		//LED_Off(0);
+		//delay();
 	}
 	
 	return 0;
