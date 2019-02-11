e6b584e25f63a715d3d80a5ed046455653ef4bfc
erhansakarya <erhansakaryaa@gmail.com>
2019-02-11 19:19:00 +0300

    stm32f1_keil_mdk was deleted.

--- stm32f1_keil_mdk/GPIO/Objects/main.d
+++
@@ -1,4 +0,0 @@
-.\objects\main.o: main.c
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\MDK-Middleware\7.7.0\Board\Board_LED.h
-.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\MDK-Middleware\7.7.0\Board\Board_Buttons.h
